256204220bb7e4aa8436bd012e98be775fc9fa58
Vinclwanzl <144253951+Vinclwanzl@users.noreply.github.com>
2024-04-08 08:56:38 +0200

    added action icons
    still contains icon dup. drag and drop bug

--- InnoLab18/Assets/Scripts/DragDrop.cs
+++ InnoLab18/Assets/Scripts/DragDrop.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     private RectTransform rectTransform;
+    private GameObject umlField;
 
+    private void Start()
+    {
+        umlField = GameObject.FindGameObjectWithTag("UMLPanel");
+    }
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -14,6 +20,18 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        //Place new Object
+        GameObject selectionPanel = GameObject.Find("SelectionPanel");
+        GridLayout grid = selectionPanel.GetComponent<GridLayout>();
+        /*GameObject newElement = gameObject;
+
+        newElement.transform.position = grid.WorldToCell(gameObject.transform.position);*/
+        //grid.GetComponent<GridLayout>().Instantiate(newElement);
+        var newElement = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        newElement.transform.parent = gameObject.transform;
+
+        //remove old Object from GridLayout
+        gameObject.transform.parent = umlField.transform;
         //Debug.Log("OnBeginDrag");
     }
 
@@ -25,6 +43,10 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (umlField.GetComponent<RectTransform>().rect.Contains(rectTransform.anchoredPosition))
+        {
+            Destroy(gameObject);
+        }
         //Debug.Log("OnEndDrag");
     }
 
