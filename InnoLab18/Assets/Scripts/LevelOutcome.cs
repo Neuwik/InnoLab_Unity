bff700789ba3a5d24a8d7c4a9746b8bbda428e84
if22b099 <if22b099@technikum-wien.at>
2024-04-23 14:41:59 +0200

    Level Outcome

---
+++ InnoLab18/Assets/Scripts/LevelOutcome.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.UI;
+using TMPro.EditorUtilities;
+using TMPro;
+
+public class LevelOutcome : MonoBehaviour
+{
+    #region DEBUG
+
+    public bool playerDied = false;
+    public bool garbageCollected = false;
+
+    #endregion
+
+    public GameManager gameManager;
+
+    public GameObject LevelOutcomeGO;
+
+    public TMP_Text LevelIndexText;
+    public TMP_Text OutcomeText;
+
+    public Button NextBtn;
+    public Button ResetBtn;
+
+    private bool _isLevelActive = true;
+    private Vector2 _posNextLevelBtn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameManager == null)
+            Debug.LogError("GameManager is empty");
+
+        LevelOutcomeGO.SetActive(false);
+
+        _posNextLevelBtn = NextBtn.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isLevelActive)
+        {
+            // Level success
+            if(!playerDied)
+            {
+                if (garbageCollected)
+                    ShowLevelOutcome(true);
+            }
+            // Level failed
+            else
+            {
+                ShowLevelOutcome(false);
+            }
+        }
+    }
+
+    private void ShowLevelOutcome(bool isSuccess)
+    {
+        _isLevelActive = false;
+        LevelOutcomeGO.SetActive(true);
+
+        // Level success
+        if (isSuccess)
+        {
+            ShowLevelSuccessUI();
+            return;
+        }
+
+        // Level failed
+        ShowLevelFailedUI();
+    }
+
+    private void ShowLevelSuccessUI()
+    {
+        OutcomeText.text = "Level complete";
+    }
+
+    private void ShowLevelFailedUI()
+    {
+        OutcomeText.text = "Level failed";
+
+        // Hide "Next" Button
+        NextBtn.gameObject.SetActive(false);
+
+        // Reset Button Position = right side
+        ResetBtn.transform.position = _posNextLevelBtn;
+    }
+}
