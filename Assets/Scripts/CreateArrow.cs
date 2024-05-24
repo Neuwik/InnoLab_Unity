f8d9c1bd0d4a6c8d699775ba04c25ab220d3349d
Florian Poppinger <florianpoppinger@gmail.com>
2024-05-24 11:34:14 +0200

    UI Fixed

--- Assets/Scripts/CreateArrow.cs
+++ Assets/Scripts/CreateArrow.cs
@@ -11,36 +11,31 @@ public class CreateArrow : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        
-        if (Actionbox.transform.parent.name == "UMLPanel")
+        switch (eventData.button)
         {
-            switch (eventData.button)
-            {
-                case PointerEventData.InputButton.Left: // Attach Arrow
-                    if (GameManager.Instance.ActiveArrow != null &&
-                        gameObject.CompareTag("UMLElement"))
-                    {
-                        GameManager.Instance.ActiveArrow.GetComponent<DrawArrow>().TargetElem = gameObject;
-                        GameManager.Instance.ActiveArrow = null;
-                    }
-                    return;
+            case PointerEventData.InputButton.Left: // Attach Arrow
+                if (GameManager.Instance.ActiveArrow != null &&
+                gameObject.CompareTag("UMLElement"))
+                {
+                    GameManager.Instance.ActiveArrow.GetComponent<DrawArrow>().TargetElem = gameObject;
+                    GameManager.Instance.ActiveArrow = null;
+                }
+                return;
 
-                case PointerEventData.InputButton.Right: // Create Arrow
-                    if(GameManager.Instance.ActiveArrow == null)
-                    {
-                        var newArrow = GameObject.Instantiate(Arrow, gameObject.transform);
-                        newArrow.transform.SetAsFirstSibling();
-                        var _ = gameObject.GetComponent<RectTransform>().rect;
-                        newArrow.GetComponent<DrawArrow>().Startpos = (Vector2)gameObject.transform.position + new Vector2(_.width / 2, _.height / 2);
-                        GameManager.Instance.ActiveArrow = newArrow;
-                    }
-                    return;
+            case PointerEventData.InputButton.Right: // Create Arrow
+                if(GameManager.Instance.ActiveArrow == null)
+                {
+                    var newArrow = GameObject.Instantiate(Arrow, gameObject.transform);
+                    newArrow.transform.SetAsFirstSibling();
+                    var _ = gameObject.GetComponent<RectTransform>().rect;
+                    newArrow.GetComponent<DrawArrow>().Startpos = (Vector2)gameObject.transform.position + new Vector2(_.width / 2, _.height / 2);
+                    GameManager.Instance.ActiveArrow = newArrow;
+                }
+                return;
 
-                case PointerEventData.InputButton.Middle: // Delete Arrow
-                    Destroy(GameManager.Instance.ActiveArrow);
-                    return;
-            }
-        } 
-            
+            case PointerEventData.InputButton.Middle: // Delete Arrow
+                Destroy(GameManager.Instance.ActiveArrow);
+                return;
+        }       
     }
 }
