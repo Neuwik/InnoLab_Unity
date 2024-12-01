48ac7d446550cd68757a811941960312004140df
Tobias Kastl <t.kastl18@gmail.com>
2024-12-01 19:02:33 +0100

    Merge branch 'TobiTitlescreen' into DEV

---
+++ Assets/Scripts/MainMenu/Play.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void OnClickSettingsButton()
+    {
+        //SceneManager.LoadScene("Settings");
+    }
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+}
